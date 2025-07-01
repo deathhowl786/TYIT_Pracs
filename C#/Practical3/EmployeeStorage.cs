7dae596c9eb16263419fc6734f65fcd2612eea42
deathhowl786 <150904502+deathhowl786@users.noreply.github.com>
2025-07-01 09:27:31 +0530

    Add files via upload

---
+++ C#/Practical3/EmployeeStorage.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical2
+{
+    public class Employee
+    {
+        public int eno;
+        public string name;
+        public string dept;
+        public int salary;
+
+        public Employee(int eno, string name, string dept, int salary) {
+            this.eno = eno;
+            this.name = name;
+            this.dept = dept;
+            this.salary = salary;
+        }
+    }
+    internal class EmployeeStorage
+    {
+
+        public static void Main(string[] args)
+        {
+            int n = 2;
+            int search = 0;
+            Employee[] employees = new Employee[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine((i + 1) + " Enter Details of Employee: ");
+                employees[i] = takeInput();
+                Console.WriteLine();
+
+                if(employees[i].salary > search) search = employees[i].salary;
+            }
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+                if (search == employees[i].salary)
+                {
+                    Console.WriteLine(string.Format("Hi My Name is {0}, My Eno is {1},I work in {2} with highest salary of {3}.", employees[i].name, employees[i].eno, employees[i].dept, employees[i].salary));
+                }
+            }
+
+        }
+
+        static public Employee takeInput()
+        {
+            Console.Write("Enter Empoyee No. : ");
+            int eno = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Name : ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Department : ");
+            string dept = Console.ReadLine();
+
+            Console.Write("Enter Salary : ");
+            int salary = int.Parse(Console.ReadLine());
+
+            return new Employee(eno, name, dept, salary);
+
+        }
+
+
+    }
+}
---
+++ C#/Practical3/StorageProgram.cs
@@ -0,0 +1,77 @@
+﻿namespace Practical2
+{
+    class Student
+    {
+        public int uid;
+        public string name;
+        public int age;
+        public string course;
+        public int marks;
+
+        public Student(int uid, string name, int age, string course, int marks)
+        {
+            this.uid = uid;
+            this.name = name;
+            this.age = age;
+            this.course = course;
+            this.marks = marks;
+        }
+
+
+    }
+
+    internal class StorageProgram
+    {
+        static void Main(string[] args)
+        {
+            int n = 2;
+            int search = 1;
+            Student[] students = new Student[n];
+
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine((i+1)+" Enter Details of Student: ");
+                students[i] = takeInput();
+                Console.WriteLine();
+            }
+            Console.Write("Enter UID to Search = ");
+            search = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i < students.Length; i++) {
+                if (search == students[i].uid) {
+                    Console.WriteLine(string.Format("Hi My Name is {0}, My UID is {1}, I am {2} years old, I study {3} in SXC and I scored {4} in it.", students[i].name, students[i].uid, students[i].age, students[i].course, students[i].marks));
+                }
+            }
+
+
+
+           
+
+
+
+
+
+        }
+
+        static public Student takeInput()
+        {
+            Console.Write("Enter UID : ");
+            int uid = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Name : ");
+            string name = Console.ReadLine(); 
+
+            Console.Write("Enter Age : ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Course : ");
+            string course = Console.ReadLine();
+
+            Console.Write("Enter Marks : ");
+            int marks = int.Parse(Console.ReadLine());
+
+            return new Student(uid, name, age, course, marks);
+
+        }
+    }
+}
