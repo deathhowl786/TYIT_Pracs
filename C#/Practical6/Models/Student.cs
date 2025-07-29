ebf371c09b7bc77e7ba98ac721f75d2a30804585
deathhowl786 <150904502+deathhowl786@users.noreply.github.com>
2025-07-29 10:07:39 +0530

    Add files via upload

---
+++ C#/Practical6/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ERP.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Za-z\s]{2,50}$", ErrorMessage = "Use letters only")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Phone must be 10 digits")]
+        public string Phone { get; set; }
+
+        [Range(1, 100)]
+        public int Age { get; set; }
+    }
+}
